3c57950b43f79e599f7b2dca9c75836f40f9a554
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-07-06 12:31:50 +0200

    implement darkmode

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "../styles/new.css"
 
 
+
 function Header() {
+    // State to track if the 'dark' class should be added
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    // Effect to toggle the 'dark' class on the html element
+    useEffect(() => {
+        if (isDarkMode) {
+        document.documentElement.classList.add('dark');
+        } else {
+        document.documentElement.classList.remove('dark');
+        }
+    }, [isDarkMode]);
+
+    const switchTheme = () => {
+        setIsDarkMode(!isDarkMode);
+    }
     return (
         <div className="app-header">
             <div className="app-header-left">
@@ -16,7 +32,7 @@ function Header() {
                         <line x1="12" y1="5" x2="12" y2="19" />
                         <line x1="5" y1="12" x2="19" y2="12" /></svg>
                 </button>
-                <button className="mode-switch" title="Switch Theme">
+                <button className="mode-switch" title="Switch Theme" onClick={switchTheme}>
                     <svg className="moon" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" width="24" height="24" viewBox="0 0 24 24">
                         <defs></defs>
                         <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
