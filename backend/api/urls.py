5d27f74d6bde59884e807b7eec3ec61630413747
Emma Schlotter <emmacal@protonmail.com>
2024-07-13 11:29:21 +0200

    changed how swagger ui is created

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import EventList, EventDetail, NoteListCreate, NoteDelete
+from . import views
+from .views import NoteListCreate,NoteDelete,EventList,EventDetail
+from rest_framework import routers
+from drf_spectacular.views import SpectacularSwaggerView,SpectacularAPIView
 
 
 urlpatterns = [
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/',SpectacularSwaggerView.as_view(url_name='schema'),name='swagger-ui'),
     path("notes/", NoteListCreate.as_view(), name="note-list"),
     path("notes/delete/<int:pk>/", NoteDelete.as_view(), name="delete-note"),
     path('events/', EventList.as_view(), name="event-list"),
     path('events/<int:pk>/', EventDetail.as_view()),
 ]
+
