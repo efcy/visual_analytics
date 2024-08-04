a45648bd0270b096ebb85e86e2f3e64a691b86db
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-08-04 11:42:42 +0200

    use pyproject toml for creating the package

--- sdk/package_pypi.sh
+++ sdk/package_pypi.sh
@@ -1,6 +1,6 @@
+python -m pip install build twine
 echo "build naoth package"
-python3 setup.py sdist bdist_wheel
-
+python -m build
 
 cat <<EOF > .pypirc
 [pypi]
@@ -11,6 +11,3 @@ EOF
 
 echo "publish naoth package to pypi"
 python3 -m twine upload dist/* --config-file .pypirc
-
-echo ""
-echo "If you got errors it might be because you are trying to overwrite an existing version. Try incrementing the version"
