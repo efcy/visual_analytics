db9970305c83f9da18d72001fe967d5d35614f43
emma <emmacal@protonmail.com>
2024-08-10 19:34:18 +0200

    added orga and user model

--- backend/user/models.py
+++ backend/user/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 from django.contrib.auth.models import AbstractUser
 
+class organization(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class vat_user(AbstractUser):
     name = models.CharField(max_length=100)
-    first_name = models.Charfield(max_length=100)
+    first_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
     token = models.CharField(max_length=100)
+    orga = models.ForeignKey(organization,on_delete=models.CASCADE,related_name='organizations')
+    
+    def __str__(self):
+        return self.first_name
