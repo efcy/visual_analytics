641799a79703bcbfd1e305df3d47cf68ef319fb2
emma schlotter <emmacal@protonmail.com>
2024-08-10 17:53:37 +0200

    added vat_user

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class vat_user(AbstractUser):
+    name = models.CharField(max_length=100)
+    first_name = models.Charfield(max_length=100)
+    email = models.EmailField(max_length=254)
+    token = models.CharField(max_length=100)
