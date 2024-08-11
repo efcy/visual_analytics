da65eedc2fb3fb8b849bb7e0666606ddeb048180
emma <emmacal@protonmail.com>
2024-08-11 22:06:27 +0200

    changed model

--- backend/user/models.py
+++ backend/user/models.py
@@ -11,7 +11,7 @@ class vat_user(AbstractUser):
     first_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
     token = models.CharField(max_length=100)
-    orga = models.ForeignKey(organization,on_delete=models.CASCADE,related_name='organizations')
+    orga = models.ForeignKey(organization,on_delete=models.CASCADE,related_name='organizations',blank=True,null=True)
     
     def __str__(self):
         return self.first_name
