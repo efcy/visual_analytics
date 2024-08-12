997aa15e3372174d1b5b2e87d336a067eb066efd
emma schlotter <emmacal@protonmail.com>
2024-08-12 10:30:51 +0200

    default admin organization for user

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import vat_user, organization
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('organization',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('organization',)}),
+    )
+
+    # Optionally, display organization in the list view
+    list_display = UserAdmin.list_display + ('organization',)
+
+admin.site.register(vat_user, CustomUserAdmin)
+admin.site.register(organization)
