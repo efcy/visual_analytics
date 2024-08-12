db189329d55982e29e1e365ee717f7f788ed3b39
emma <emmacal@protonmail.com>
2024-08-12 15:49:41 +0200

    clean up

--- backend/user/tests.py
+++ backend/user/tests.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from django.contrib.auth import get_user_model
 from .models import organization
-
+#not working
 User = get_user_model()
 
 class AuthViewsTestCase(TestCase):
