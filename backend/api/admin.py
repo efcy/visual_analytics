49d5eff96821e69f8f53fb3163cccf8f659b09dd
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-08-19 12:29:11 +0200

    make it possible to quickly search for image paths in django admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Event, Game, RobotData, Image, SensorLog
 
+
+class ImageAdmin(admin.ModelAdmin):
+    search_fields = ['image_url__icontains']
+
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(Game)
 admin.site.register(RobotData)
-admin.site.register(Image)
+admin.site.register(Image, ImageAdmin)
 admin.site.register(SensorLog)
