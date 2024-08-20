761ab9a387dc34b230da96e14c9b8dd68337132b
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-08-20 23:37:02 +0200

    improve representation view

--- web/src/components/custom/AnnotationView/AnnotationView.jsx
+++ web/src/components/custom/AnnotationView/AnnotationView.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useParams, Link } from "react-router-dom";
 import MultiRowRangeSlider from "../MultiRowRangeSlider/MultiRowRangeSlider";
 import { useSelector, useDispatch } from "react-redux";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import CanvasView from "../CanvasView.jsx";
 import {
   Accordion,
@@ -10,7 +11,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import classes from './AnnotationView.module.css'
+import classes from "./AnnotationView.module.css";
 
 const AnnotationView = () => {
   const [images, setImages] = useState([]);
@@ -29,9 +30,10 @@ const AnnotationView = () => {
     if (!images[store_idx]) {
       return;
     }
-    const new_url = 'https://logs.berlin-united.com/' + images[store_idx].image_url
-    console.log("url: ", new_url)
-    setUrl(new_url)
+    const new_url =
+      "https://logs.berlin-united.com/" + images[store_idx].image_url;
+    console.log("url: ", new_url);
+    setUrl(new_url);
   }, [store_idx, images]);
 
   useEffect(() => {
@@ -45,7 +47,9 @@ const AnnotationView = () => {
 
   const getImages = () => {
     axios
-      .get(`${import.meta.env.VITE_API_URL}/api/image?log=${id}&camera=${camera}`)
+      .get(
+        `${import.meta.env.VITE_API_URL}/api/image?log=${id}&camera=${camera}`
+      )
       .then((res) => res.data)
       .then((data) => {
         setImages(data);
@@ -58,11 +62,11 @@ const AnnotationView = () => {
     if (!image_db_obj) {
       return;
     }
-    const new_url = 'https://logs.berlin-united.com/' + image_db_obj.image_url
+    const new_url = "https://logs.berlin-united.com/" + image_db_obj.image_url;
     if (!loadedImages[new_url]) {
       const img = new Image();
       img.src = new_url;
-      img.onload = () =>  
+      img.onload = () =>
         setLoadedImages((prev) => ({ ...prev, [new_url]: img }));
     }
   };
@@ -71,7 +75,7 @@ const AnnotationView = () => {
     if (!image_db_obj) {
       return;
     }
-    const new_url = 'https://logs.berlin-united.com/' + image_db_obj.image_url
+    const new_url = "https://logs.berlin-united.com/" + image_db_obj.image_url;
     console.log(new_url);
     //const url = image_db_obj.image_url
     if (loadedImages[new_url]) {
@@ -86,32 +90,60 @@ const AnnotationView = () => {
     }
   };
 
+  // FIXME fix height of representation view
 
   return (
     <div className={classes.mainView}>
       <div className={classes.dataView}>
         <CanvasView imageUrl={url} />
         <div className="representationSelector">
-          <Accordion type="single" collapsible>
-            <AccordionItem value="item-1">
-              <AccordionTrigger>Is it accessible?</AccordionTrigger>
-              <AccordionContent>
-                Yes. It adheres to the WAI-ARIA design pattern.
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-        </div>
-        <div>
-          <code>
-            asdhoashdohiaosdijoaijsdasosuhdpuasiogdlbaiwhep 
-          </code>
+          <Tabs defaultValue="MultiBallPercept" className="tabs-container">
+            <TabsList className="grid h-full grid-rows tabs-list">
+              <TabsTrigger value="MultiBallPercept">MultiBallPercept</TabsTrigger>
+              <TabsTrigger value="BallModel">BallModel</TabsTrigger>
+              <TabsTrigger value="TeamState">TeamState</TabsTrigger>
+              <TabsTrigger value="ScanLineEdgelPercept">ScanLineEdgelPercept</TabsTrigger>
+              <TabsTrigger value="FSRData">FSRData</TabsTrigger>
+              <TabsTrigger value="BallCandidatesTop">BallCandidatesTop</TabsTrigger>
+              <TabsTrigger value="CameraMatrixTop">CameraMatrixTop</TabsTrigger>
+              <TabsTrigger value="TeamMessageDecision">TeamMessageDecision</TabsTrigger>
+              <TabsTrigger value="MotionStatus">MotionStatus</TabsTrigger>
+              <TabsTrigger value="ShortLinePercept">ShortLinePercept</TabsTrigger>
+              <TabsTrigger value="RansacLinePercept">RansacLinePercept</TabsTrigger>
+              <TabsTrigger value="ScanLineEdgelPerceptTop">ScanLineEdgelPerceptTop</TabsTrigger>
+              <TabsTrigger value="InertialSensorData">InertialSensorData</TabsTrigger>
+              <TabsTrigger value="RobotInfo">RobotInfo</TabsTrigger>
+              <TabsTrigger value="GoalPercept">GoalPercept</TabsTrigger>
+              <TabsTrigger value="FieldPerceptTop">FieldPerceptTop</TabsTrigger>
+              <TabsTrigger value="MotorJointData">MotorJointData</TabsTrigger>
+              <TabsTrigger value="CameraMatrix">CameraMatrix</TabsTrigger>
+              <TabsTrigger value="SensorJointData">SensorJointData</TabsTrigger>
+              <TabsTrigger value="ButtonData">ButtonData</TabsTrigger>
+              <TabsTrigger value="RansacCirclePercept2018">RansacCirclePercept2018</TabsTrigger>
+              <TabsTrigger value="FieldPercept">FieldPercept</TabsTrigger>
+              <TabsTrigger value="AudioData">AudioData</TabsTrigger>
+              <TabsTrigger value="GoalPerceptTop">GoalPerceptTop</TabsTrigger>
+              <TabsTrigger value="BallCandidates">BallCandidates</TabsTrigger>
+              <TabsTrigger value="OdometryData">OdometryData</TabsTrigger>
+              <TabsTrigger value="FrameInfo">FrameInfo</TabsTrigger>
+              <TabsTrigger value="AccelerometerData">AccelerometerData</TabsTrigger>
+              <TabsTrigger value="IMUData">IMUData</TabsTrigger>
+              <TabsTrigger value="GyrometerData">GyrometerData</TabsTrigger>
+            </TabsList>
+            <TabsContent value="MultiBallPercept" className="tabs-content">
+            MultiBallPercept Data
+            </TabsContent>
+            <TabsContent value="GyrometerData">
+              Change your password here.
+            </TabsContent>
+          </Tabs>
         </div>
       </div>
 
       <div className="p-4">
-        <button onClick={() =>  setCamera("TOP")}>TOP</button>
-        <button onClick={() =>  setCamera("BOTTOM")}>BOTTOM</button>
-        <MultiRowRangeSlider length={images.length}/>
+        <button onClick={() => setCamera("TOP")}>TOP</button>
+        <button onClick={() => setCamera("BOTTOM")}>BOTTOM</button>
+        <MultiRowRangeSlider length={images.length} />
       </div>
     </div>
   );
