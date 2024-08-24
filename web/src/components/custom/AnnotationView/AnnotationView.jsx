484658a77779c59044b499596d6d6137153d40e5
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-08-25 00:18:03 +0200

    cleanup annotation view

--- web/src/components/custom/AnnotationView/AnnotationView.jsx
+++ web/src/components/custom/AnnotationView/AnnotationView.jsx
@@ -9,81 +9,37 @@ import CanvasView from "../CanvasView/CanvasView.jsx";
 import classes from "./AnnotationView.module.css";
 
 const AnnotationView = () => {
-  const [images, setImages] = useState([]);
+  const [imageList, setImageList] = useState([]);
   const [camera, setCamera] = useState("BOTTOM");
-  const [loadedImages, setLoadedImages] = useState({});
   const [url, setUrl] = useState("");
   const { id } = useParams();
   const store_idx = useSelector((state) => state.canvasReducer.index);
 
   useEffect(() => {
-    getImages();
+    get_image_data();
   }, [camera]); // this list is called dependency array
 
-  useEffect(() => {
-    //loadImage(images[store_idx]);
-    if (!images[store_idx]) {
-      return;
-    }
-    const new_url =
-      "https://logs.berlin-united.com/" + images[store_idx].image_url;
-    console.log("url: ", new_url);
-    setUrl(new_url);
-  }, [store_idx, images]);
-
-  useEffect(() => {
-    // Preload current image and next few images
-    for (let i = 0; i < 5; i++) {
-      if (images[store_idx + i]) {
-        preloadImage(images[store_idx + i]);
-      }
-    }
-  }, [store_idx, images]);
-
-  const getImages = () => {
+  const get_image_data = () => {
     axios
       .get(
         `${import.meta.env.VITE_API_URL}/api/image?log=${id}&camera=${camera}`
       )
       .then((res) => res.data)
       .then((data) => {
-        setImages(data);
+        setImageList(data);
         console.log("Image List", data);
       })
       .catch((err) => alert(err));
   };
 
-  const preloadImage = (image_db_obj) => {
-    if (!image_db_obj) {
+  useEffect(() => { 
+    if (!imageList[store_idx]) {
       return;
     }
-    const new_url = "https://logs.berlin-united.com/" + image_db_obj.image_url;
-    if (!loadedImages[new_url]) {
-      const img = new Image();
-      img.src = new_url;
-      img.onload = () =>
-        setLoadedImages((prev) => ({ ...prev, [new_url]: img }));
-    }
-  };
-
-  const loadImage = (image_db_obj) => {
-    if (!image_db_obj) {
-      return;
-    }
-    const new_url = "https://logs.berlin-united.com/" + image_db_obj.image_url;
-    console.log(new_url);
-    //const url = image_db_obj.image_url
-    if (loadedImages[new_url]) {
-      //drawImageOnCanvas(loadedImages[new_url]);
-    } else {
-      const img = new Image();
-      img.onload = () => {
-        setLoadedImages((prev) => ({ ...prev, [new_url]: img }));
-        //drawImageOnCanvas(img);
-      };
-      img.src = new_url;
-    }
-  };
+    const new_url =
+      "https://logs.berlin-united.com/" + imageList[store_idx].image_url;
+    setUrl(new_url);
+  }, [store_idx, imageList]);
 
   return (
     <div className={classes.mainView}>
@@ -93,7 +49,7 @@ const AnnotationView = () => {
       </div>
 
       <div className="p-4">
-        <MultiRowRangeSlider length={images.length} />
+        <MultiRowRangeSlider length={imageList.length} />
       </div>
     </div>
   );
