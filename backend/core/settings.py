a650bc986b32d4b3e1c09f9aefd49ae3c6013078
emma <emmacal@protonmail.com>
2024-08-24 10:51:00 +0200

    added even more descripition and ordering of settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from dotenv import load_dotenv
 import os
 
+#loads a .env file. There is no .env file ?
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,6 +26,8 @@ INTERNAL_IPS = [
 #matches with host headers in requests
 ALLOWED_HOSTS = ["*"]
 
+#configures default authentication and permissions
+#users need to authenticate with session or token to use any endpoint
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         'rest_framework.authentication.SessionAuthentication',
@@ -33,6 +36,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    #generates API documentation based on the OpenAPI 3.0 standard.
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
@@ -52,7 +56,9 @@ INSTALLED_APPS = [
     'user',
     #'django.contrib.gis',
 ]
-
+#registers middleware components
+#these components process requests before reaching or leaving a view
+#the order is very important in this list for more see https://docs.djangoproject.com/en/5.1/ref/middleware/#middleware-ordering
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -63,9 +69,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+#defines where the url patterns are defined
+#'core.urls' means urls.py in the core app
 ROOT_URLCONF = 'core.urls'
 
+#template stuff. maybe not even requiered since we don't use templates
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -82,11 +90,13 @@ TEMPLATES = [
     },
 ]
 
+#specifies location of wsgi application
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+# loads env variables for database connection
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -101,6 +111,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
+#defines validators for new user passwords
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -137,18 +148,25 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # https://github.com/adamchainz/django-cors-headers
-CORS_ALLOW_ALL_ORIGINS = True
-CSRF_COOKIE_DOMAIN = ".berlin-united.com"
+CORS_ALLOW_ALL_ORIGINS = True #we shouldn't to this because it makes the cors allowed origins obsolete
 CORS_ALLOWS_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = ['https://api.berlin-united.com','https://vat.berlin-united.com', 'http://localhost:5173', 'http://localhost:8000']
+
+#makes csrf cookie valid on all subdomains
+CSRF_COOKIE_DOMAIN = ".berlin-united.com"
+#specifies all domains where django accepts POST requests from with CSRF tokens
 CSRF_TRUSTED_ORIGINS = ['https://api.berlin-united.com', 'https://vat.berlin-united.com', 'http://localhost:8000', 'http://localhost:5173']
 
-
+#requiered if there is a loadbalancer in front of django that forwards requests over http
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CORS_ALLOWED_ORIGINS = ['https://api.berlin-united.com','https://vat.berlin-united.com', 'http://localhost:5173', 'http://localhost:8000']
-CORS_ALLOW_CREDENTIALS = True
+
+#we only want tokenauth in the swagger view
 SPECTACULAR_SETTINGS = {
     'AUTHENTICATION_WHITELIST': ['rest_framework.authentication.TokenAuthentication'],
 }
+
+#registers our custom user model
 AUTH_USER_MODEL = 'user.VATUser' 
 
+#maximum fields allowed in one post request
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 30240
