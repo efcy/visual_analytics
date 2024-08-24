83496bbaf4647daa6dbd0553e8bfcd8d00838680
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-08-24 18:26:49 +0200

    improve dataview

--- web/src/components/custom/AnnotationView/AnnotationView.jsx
+++ web/src/components/custom/AnnotationView/AnnotationView.jsx
@@ -1,16 +1,10 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import MultiRowRangeSlider from "../MultiRowRangeSlider/MultiRowRangeSlider";
-import { useSelector, useDispatch } from "react-redux";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useSelector } from "react-redux";
+import DataView from "../DataView/DataView.jsx";
 import CanvasView from "../CanvasView.jsx";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
 import classes from "./AnnotationView.module.css";
 
 const AnnotationView = () => {
@@ -90,54 +84,11 @@ const AnnotationView = () => {
     }
   };
 
-  // FIXME fix height of representation view
-
   return (
     <div className={classes.mainView}>
       <div className={classes.dataView}>
         <CanvasView imageUrl={url} />
-        <div className="representationSelector">
-          <Tabs defaultValue="MultiBallPercept" className="tabs-container">
-            <TabsList className="grid h-full grid-rows tabs-list">
-              <TabsTrigger value="MultiBallPercept">MultiBallPercept</TabsTrigger>
-              <TabsTrigger value="BallModel">BallModel</TabsTrigger>
-              <TabsTrigger value="TeamState">TeamState</TabsTrigger>
-              <TabsTrigger value="ScanLineEdgelPercept">ScanLineEdgelPercept</TabsTrigger>
-              <TabsTrigger value="FSRData">FSRData</TabsTrigger>
-              <TabsTrigger value="BallCandidatesTop">BallCandidatesTop</TabsTrigger>
-              <TabsTrigger value="CameraMatrixTop">CameraMatrixTop</TabsTrigger>
-              <TabsTrigger value="TeamMessageDecision">TeamMessageDecision</TabsTrigger>
-              <TabsTrigger value="MotionStatus">MotionStatus</TabsTrigger>
-              <TabsTrigger value="ShortLinePercept">ShortLinePercept</TabsTrigger>
-              <TabsTrigger value="RansacLinePercept">RansacLinePercept</TabsTrigger>
-              <TabsTrigger value="ScanLineEdgelPerceptTop">ScanLineEdgelPerceptTop</TabsTrigger>
-              <TabsTrigger value="InertialSensorData">InertialSensorData</TabsTrigger>
-              <TabsTrigger value="RobotInfo">RobotInfo</TabsTrigger>
-              <TabsTrigger value="GoalPercept">GoalPercept</TabsTrigger>
-              <TabsTrigger value="FieldPerceptTop">FieldPerceptTop</TabsTrigger>
-              <TabsTrigger value="MotorJointData">MotorJointData</TabsTrigger>
-              <TabsTrigger value="CameraMatrix">CameraMatrix</TabsTrigger>
-              <TabsTrigger value="SensorJointData">SensorJointData</TabsTrigger>
-              <TabsTrigger value="ButtonData">ButtonData</TabsTrigger>
-              <TabsTrigger value="RansacCirclePercept2018">RansacCirclePercept2018</TabsTrigger>
-              <TabsTrigger value="FieldPercept">FieldPercept</TabsTrigger>
-              <TabsTrigger value="AudioData">AudioData</TabsTrigger>
-              <TabsTrigger value="GoalPerceptTop">GoalPerceptTop</TabsTrigger>
-              <TabsTrigger value="BallCandidates">BallCandidates</TabsTrigger>
-              <TabsTrigger value="OdometryData">OdometryData</TabsTrigger>
-              <TabsTrigger value="FrameInfo">FrameInfo</TabsTrigger>
-              <TabsTrigger value="AccelerometerData">AccelerometerData</TabsTrigger>
-              <TabsTrigger value="IMUData">IMUData</TabsTrigger>
-              <TabsTrigger value="GyrometerData">GyrometerData</TabsTrigger>
-            </TabsList>
-            <TabsContent value="MultiBallPercept" className="tabs-content">
-            MultiBallPercept Data
-            </TabsContent>
-            <TabsContent value="GyrometerData">
-              Change your password here.
-            </TabsContent>
-          </Tabs>
-        </div>
+        <DataView />
       </div>
 
       <div className="p-4">
