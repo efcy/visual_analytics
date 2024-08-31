d8f461152eafaaaa9ec7b88347da3e8ac3137235
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-08-31 17:37:13 +0200

    cleanup sidebar component

--- web/src/components/custom/Sidebar/Sidebar.jsx
+++ web/src/components/custom/Sidebar/Sidebar.jsx
@@ -1,16 +1,9 @@
 import React from "react";
-import "@/styles/new.css";
-import { Routes, Route, Link, Outlet } from "react-router-dom";
-import { MdEvent, MdOutlineSettings } from "react-icons/md";
+
+import { Link, } from "react-router-dom";
+import { MdEvent } from "react-icons/md";
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+
 // icon list: https://www.radix-ui.com/icons
 import { GearIcon, ExitIcon } from "@radix-ui/react-icons"
 
@@ -31,38 +24,6 @@ function Sidebar() {
           <ExitIcon className="h-4 w-4" />
         </Link>
       </Button>
-    </div>
-  );
-}
-
-function Sidebar_w_dialog() {
-  return (
-    <div className="app-sidebar">
-      <Link to="/" className="app-sidebar-link">
-        <MdEvent />
-      </Link>
-      Events
-      <a href="" className="app-sidebar-link">
-        <MdOutlineSettings />
-      </a>
-      Settings
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="ghost" size="icon">
-            <GearIcon className="h-4 w-4" />
-          </Button>
-          
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Are you sure absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
     </div>
   );
 }
