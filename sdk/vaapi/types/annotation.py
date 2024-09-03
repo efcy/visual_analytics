879a8d89c6d4c6bd6a2b7536dde260b3a8b47f01
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-09-03 16:30:56 +0200

    create pydantic models for all of our types

--- sdk/vaapi/types/annotation.py
+++ sdk/vaapi/types/annotation.py
@@ -7,92 +7,30 @@ from .annotation_last_action import AnnotationLastAction
 
 
 class Annotation(pydantic_v1.BaseModel):
+    """
+    Id assigned by django
+    """
     id: typing.Optional[int] = None
+
+    """
+    List of annotation results for the task
+    FIXME: 
+    """
     result: typing.Optional[typing.List[typing.Dict[str, typing.Any]]] = pydantic_v1.Field(default=None)
+    
     """
     List of annotation results for the task
     """
-
-    created_username: typing.Optional[str] = pydantic_v1.Field(default=None)
-    """
-    Username string
-    """
-
-    created_ago: typing.Optional[str] = pydantic_v1.Field(default=None)
-    """
-    Time delta from creation time
-    """
-
-    completed_by: typing.Optional[int] = None
     unique_id: typing.Optional[str] = None
-    was_cancelled: typing.Optional[bool] = pydantic_v1.Field(default=None)
-    """
-    User skipped the task
-    """
 
-    ground_truth: typing.Optional[bool] = pydantic_v1.Field(default=None)
-    """
-    This annotation is a Ground Truth (ground_truth)
-    """
-
-    created_at: typing.Optional[dt.datetime] = pydantic_v1.Field(default=None)
-    """
-    Creation time
-    """
-
-    updated_at: typing.Optional[dt.datetime] = pydantic_v1.Field(default=None)
-    """
-    Last updated time
-    """
-
-    draft_created_at: typing.Optional[dt.datetime] = pydantic_v1.Field(default=None)
-    """
-    Draft creation time
-    """
-
-    lead_time: typing.Optional[float] = pydantic_v1.Field(default=None)
-    """
-    How much time it took to annotate the task
-    """
-
-    import_id: typing.Optional[int] = pydantic_v1.Field(default=None)
-    """
-    Original annotation ID that was at the import step or NULL if this annotation wasn't imported
-    """
-
-    last_action: typing.Optional[AnnotationLastAction] = pydantic_v1.Field(default=None)
-    """
-    Action which was performed in the last annotation history item
-    """
-
-    task: typing.Optional[int] = pydantic_v1.Field(default=None)
+    image: typing.Optional[int] = pydantic_v1.Field(default=None)
     """
     Corresponding task for this annotation
     """
 
-    project: typing.Optional[int] = pydantic_v1.Field(default=None)
+    robot_data: typing.Optional[int] = pydantic_v1.Field(default=None)
     """
-    Project ID for this annotation
-    """
-
-    updated_by: typing.Optional[int] = pydantic_v1.Field(default=None)
-    """
-    Last user who updated this annotation
-    """
-
-    parent_prediction: typing.Optional[int] = pydantic_v1.Field(default=None)
-    """
-    Points to the prediction from which this annotation was created
-    """
-
-    parent_annotation: typing.Optional[int] = pydantic_v1.Field(default=None)
-    """
-    Points to the parent annotation from which this annotation was created
-    """
-
-    last_created_by: typing.Optional[int] = pydantic_v1.Field(default=None)
-    """
-    User who created the last annotation history item
+    robot_data id for this annotation, it will get the robot data id from the image
     """
 
     def json(self, **kwargs: typing.Any) -> str:
