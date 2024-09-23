936c6dc3f1817b42eb1460e608c43fc37fec11b6
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-09-23 23:40:21 +0200

    add cognition repr to admin panel

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -26,6 +26,7 @@ router.register('events',views.EventViewSet)
 router.register('games', views.GameViewSet)
 router.register('robotdata',views.RobotDataViewSet)
 router.register('image',views.ImageViewSet)
+router.register("cognitionrepr",views.CognitionRepresentationViewSet)
 router.register("motionrepr",views.MotionRepresentationViewSet)
 
 urlpatterns += router.urls
