82995a4ae0bb96de574dda5915d43fa9fd1803db
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-09-24 13:04:47 +0200

    refactor the tests

---
+++ sdk/vaapi/run_tests.py
@@ -0,0 +1,11 @@
+from vaapi.client import Vaapi
+from tests.event_tests import basic_event_tests
+if __name__ == "__main__":
+    client = Vaapi(
+        base_url='http://127.0.0.1:8000/',  
+        api_key="84c6f4b516cc9d292f1b0eba26ea88e99812fbb9",
+    )
+    a = client.annotations.get(id=1)
+    print(a)
+
+    basic_event_tests(client)
--- sdk/vaapi/tests/event_tests.py
+++ sdk/vaapi/tests/event_tests.py
@@ -1,10 +1,4 @@
-from vaapi.client import Vaapi
-
-if __name__ == "__main__":
-    client = Vaapi(
-    base_url='http://127.0.0.1:8000/',  
-    api_key="84c6f4b516cc9d292f1b0eba26ea88e99812fbb9",
-)
+def basic_event_tests(client):
     a = client.events.create(name="test_event")
     print(a)
 
