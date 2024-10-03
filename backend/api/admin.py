8c695f58ed7512631ed71f00dbaaf5d528d772ab
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-10-03 14:26:48 +0200

    improve admin panel even more

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -34,18 +34,29 @@ class GameAdmin(admin.ModelAdmin):
 
 class LogAdmin(admin.ModelAdmin):
     search_fields = ['game_id__team1__icontains', 'game_id__team2__icontains', "head_number","player_number"]
-    list_display = ["get_game_id", "get_team1", "get_team2", "player_number", "head_number"]
+    list_display = ["get_game_id", "get_id", "get_start_time", "get_team1", "get_team2", "player_number", "head_number", "is_test"]
 
     def get_game_id(self, obj):
         return obj.game_id.id
 
+    def get_id(self, obj):
+        return obj.id
+    
+    def get_start_time(self, obj):
+        return obj.game_id.start_time
+    
     def get_team1(self, obj):
         return obj.game_id.team1
     
     def get_team2(self, obj):
         return obj.game_id.team2
+
+    def is_test(self, obj):
+        return obj.game_id.is_testgame
     
+    get_id.short_description = 'Log ID'
     get_game_id.short_description = 'Game ID'
+    get_start_time.short_description = 'Time'
     get_team1.short_description = 'Team 1'
     get_team2.short_description = 'Team 2'
 
