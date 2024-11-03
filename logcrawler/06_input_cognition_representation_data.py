6185da0058ef32ab3460b653c03933f5d9aa086f
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-11-03 12:24:19 +0100

    cleanup logcrawler

--- logcrawler/06_input_cognition_representation_data.py
+++ logcrawler/06_input_cognition_representation_data.py
@@ -67,8 +67,25 @@ if __name__ == "__main__":
         log_path = Path(log_root_path) / data.log_path
         print("log_path: ", log_path)
 
-        representation_list = ["BallModel", "BallCandidates", "BallCandidatesTop", "CameraMatrix", "CameraMatrixTop", "OdometryData", "FieldPercept", "FieldPerceptTop", "GoalPercept", "GoalPerceptTop", "RansacLinePercept", "ShortLinePercept", "ScanLineEdgelPercept", "ScanLineEdgelPerceptTop", "RansacCirclePercept2018"]
-        representation_list = ["BallCandidates", "BallCandidatesTop", "MultiBallPercept"]
+        representation_list = [
+            "BallModel",
+            "BallCandidates",
+            "BallCandidatesTop",
+            "CameraMatrix",
+            "CameraMatrixTop",
+            "OdometryData",
+            "FieldPercept",
+            "FieldPerceptTop",
+            "GoalPercept",
+            "GoalPerceptTop",
+            "MultiBallPercept"
+            "RansacLinePercept", 
+            "ShortLinePercept",
+            "ScanLineEdgelPercept",
+            "ScanLineEdgelPerceptTop",
+            "RansacCirclePercept2018"
+        ]
+
         # check if we need to insert this log
         new_representation_list = is_input_done(representation_list)
         if not args.force and len(new_representation_list) == 0:
--- logcrawler/README.md
+++ logcrawler/README.md
@@ -1,9 +1,20 @@
 # Logcrawler
+Scripts for inserting log data into the database. These scripts generally only need to be run once. 
 
+The logcrawler scripts use the [VAAPI pip package](https://pypi.org/project/vaapi/) to communicate with the backend.
 
-
-## Setup
-Make sure you have the log folder mounted via sshfs. 
+You will need these environment variables in order to run the scripts:
 ```bash
-export VAT_LOG_ROOT=<path to folder containing all the events>
+VAT_LOG_ROOT=<"path to folder containing all the events">
+VAT_API_URL=<"http://127.0.0.1:8000/ or https://api.berlin-united.com/">
+VAT_API_TOKEN=<token you can get from the website>
 ```
+
+## Access the log folder
+If you have a large disk you can download the log folders in the correct structure to your disk and use the logs locally. This is recommended if you want to add a whole event to the database.
+
+TODO: write scripts that downloads all necessary files
+
+Alternatively you can use sshfs. This is much slower then using local files.
+
+TODO add sshfs tutorial here
