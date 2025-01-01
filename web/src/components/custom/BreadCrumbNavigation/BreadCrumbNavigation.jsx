5234732000794d230b32a990af9587105c368df2
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-01-01 21:11:00 +0100

    small cleanup

--- web/src/components/custom/BreadCrumbNavigation/BreadCrumbNavigation.jsx
+++ web/src/components/custom/BreadCrumbNavigation/BreadCrumbNavigation.jsx
@@ -1,4 +1,4 @@
-import { useParams, Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useGames } from "@/hooks/useGames";
 import { useLog, useLogs } from "@/hooks/useLogs";
 
