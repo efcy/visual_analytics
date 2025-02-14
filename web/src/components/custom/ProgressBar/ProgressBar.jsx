b91b97eb5e20090ccfe6ff8fe103cfabe33fb122
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-02-14 11:39:55 +0100

    remove react code

--- web/src/components/custom/ProgressBar/ProgressBar.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "./ProgressBar.module.css";
-
-export const ProgressBar = ({ value, maxValue = 100 }) => (
-  <progress className={classes.progress_bar} value={value} max={maxValue}>
-    {value}%
-  </progress>
-);
