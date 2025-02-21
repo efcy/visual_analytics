38217556037e1f984d69903e620f48d45eb32f47
emma schlotter <simon@schlotterfam.de>
2025-02-21 06:56:49 +0100

    using enviroment variables

--- logcrawler/query_example.py
+++ logcrawler/query_example.py
@@ -1,10 +1,11 @@
 
+import os
 from vaapi.client import VATClient
 
 client = VATClient(
-    base_url="http://127.0.0.1:8000/",
-    api_key="222ca62d70706ed2ff65afe21ca3475ff23f3b05",
-)
+        base_url=os.environ.get("VAT_API_URL"),
+        api_key=os.environ.get("VAT_API_TOKEN"),
+    )
 
 query = """
 query{
